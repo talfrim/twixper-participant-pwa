9e47adffb5d6dc23f81c8872d19fe9dc5a6bd8b4
talfrim <49164423+talfrim@users.noreply.github.com>
2020-10-15 01:23:04 +0300

    added hollow post button in feed

--- src/components/post/PostButtonAndModal.vue
+++ src/components/post/PostButtonAndModal.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <button type="button" class="post-btn">
-            +<i class="fas fa-feather-alt"></i> 
+        <button type="button" class="post-btn" v-on:click="openPostModal">
+            <i class="fas fa-feather-alt"></i> 
         </button>
     </div>
 </template>
@@ -13,5 +13,22 @@
 </script>
 
 <style scoped>
+
+    .post-btn {
+		position: fixed;;
+		bottom: 2rem;
+		left: 72%;
+		width: 7rem;
+		padding: 1.5rem 1rem;
+		background-color: #1aa1f5;
+		color: #fff;
+		font-size: 2rem;
+		border: 0.1rem solid #1aa1f5;
+		border-radius: 3rem;
+		cursor: pointer;
+		box-shadow: 0 0.3rem 1rem rgba(0, 0, 0, 0.3);
+		transition: background-color 0.3s;
+	}
+
 
 </style>
--- src/pages/tweets_display/FeedPage.vue
+++ src/pages/tweets_display/FeedPage.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
+        <PostButton></PostButton>
         <TweetPreviewList></TweetPreviewList>
     </div>
 </template>
 
 <script>
     import TweetPreviewList from "../../components/tweets_display/TweetPreviewList.vue"
+    import PostButton from "../../components/post/PostButtonAndModal.vue"
     export default {
-        components: {TweetPreviewList}
+        components: {TweetPreviewList, PostButton}
     }
-
-    
 </script>
 
 <style>

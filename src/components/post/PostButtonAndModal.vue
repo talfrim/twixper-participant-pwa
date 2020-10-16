2db16018b09c13c867b21d1e6445adf89367c33c
talfrim <49164423+talfrim@users.noreply.github.com>
2020-10-17 00:58:02 +0300

    removed some unused cdd

--- src/components/post/PostButtonAndModal.vue
+++ src/components/post/PostButtonAndModal.vue
@@ -11,21 +11,7 @@
 
 <style scoped>
 
-    .post-btn {
-		position: fixed;;
-		bottom: 2rem;
-		left: 72%;
-		width: 7rem;
-		padding: 1.5rem 1rem;
-		background-color: #1aa1f5;
-		color: #fff;
-		font-size: 2rem;
-		border: 0.1rem solid #1aa1f5;
-		border-radius: 3rem;
-		cursor: pointer;
-		box-shadow: 0 0.3rem 1rem rgba(0, 0, 0, 0.3);
-		transition: background-color 0.3s;
-	}
+
 
 
 </style>

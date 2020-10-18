8265bbc3bcd486b510e3d02fa828ba1078c6b3b7
talfrim <49164423+talfrim@users.noreply.github.com>
2020-10-18 16:18:42 +0300

    build

---
+++ dist/js/chunk-2d0e9b63.33d0042a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e9b63"],{"8f65":function(n,u,e){"use strict";e.r(u);var l,c,o=e("2877"),p={},s=Object(o["a"])(p,l,c,!1,null,null,null);u["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0e9b63.33d0042a.js.map

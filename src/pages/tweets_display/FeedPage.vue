a2fbd7426f1c012f44b29b0c8a955bac34dcca6e
talfrim <49164423+talfrim@users.noreply.github.com>
2020-10-19 23:07:31 +0300

    comment on req

--- src/pages/tweets_display/FeedPage.vue
+++ src/pages/tweets_display/FeedPage.vue
@@ -25,7 +25,7 @@ export default {
     },
     mounted(){
         this.myEl = this.$el;
-        twtCommunicator.getHomeTimeline();
+        //twtCommunicator.getHomeTimeline();
     }
 }
 </script>

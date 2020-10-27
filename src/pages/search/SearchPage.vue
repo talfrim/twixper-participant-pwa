b878693f37d4401647754e9bf26dbd7de0e1f559
talfrim <49164423+talfrim@users.noreply.github.com>
2020-10-27 16:15:52 +0200

    more search

--- src/pages/search/SearchPage.vue
+++ src/pages/search/SearchPage.vue
@@ -2,19 +2,24 @@
     <div>
 
         <div class="col-md-6">
-            <b-form-input v-model="query" size="lg" id="searchInput" autocomplete="off" v-on:keyup.enter="searchClicked" placeholder="Search tweets or users" list="last-search"></b-form-input>
-            <datalist id="last-search">
-              <option> lastSearchFromStore </option>
-            </datalist>
+            <div class="page-top">
+                <b-form-input v-model="query" size="lg" id="searchInput" autocomplete="off" v-on:keyup.enter="searchClicked" placeholder="Search tweets or users" list="last-search"></b-form-input>
+                <datalist id="last-search">
+                <option> Last Search From Store </option>
+                </datalist>
+            </div>
         </div>
         <div>
-            <b-tabs content-class="mt-3" justified>
+            <b-tabs class="tabs" content-class="mt-3" justified>
                 <b-tab v-if="hasTweets" title="TWEETS" active>
+                    <Loader v-if="this.loadingTweets"/>
                     <TweetPreviewList :feedTweetsArr="this.tweetResults"></TweetPreviewList>
                 </b-tab>
                 <b-tab v-else title="TWEETS" disabled> </b-tab> <!-- disabled tweets tab-->
-                <b-tab v-if="hasPeople" title="USERS" >
-                    <p>second tab</p>\
+                <b-tab v-if="hasUsers" title="USERS" >
+                    <p>users tab</p>
+                    <Loader v-if="this.loadingUsers"/>
+
                   </b-tab>    
                 <b-tab v-else title="USERS" disabled> </b-tab>  <!-- disabled users tab-->
             </b-tabs>
@@ -28,24 +33,28 @@
 import MenuHeader from "../../components/MenuHeader.vue"
 import TweetPreviewList from "../../components/tweets_display/TweetPreviewList.vue"
 import feedJSON from "../../communicators/FeedJSON.js"
+import Loader from "../../components/Loader.vue";
 
 
 export default {
     components: {
         MenuHeader,
         TweetPreviewList,
+        Loader,
     },
     data(){
         return{
             myEl: null,
             query:"",
             tweetResults:[],
-            peopleResults:[],
+            usersResults:[],
+            loadingTweets: false,
+            loadingUsers: false,
         }
     },
     computed: {
         hasTweets() {return this.tweetResults.length>0},
-        hasPeople() {return this.peopleResults.length>0}
+        hasUsers() {return this.usersResults.length>0}
     },
     mounted() {
         this.myEl = this.$el;
@@ -53,14 +62,33 @@ export default {
     },
     methods: {
         searchClicked() {
-            console.log("search clicked")
-            this.tweetResults.push(...feedJSON);
+            this.clearResults() 
+            this.loadingTweets=true
+            this.loadingUsers=true
+            this.tweetResults.push(...feedJSON) //should be done with await communicator.search(query)...
+            this.loadingTweets=false
+            this.loadingUsers=false
+
+        },
+        sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        },
+        clearResults() {
+            this.tweetResults = []
+            this.usersResults = []
         }
     }
 }
 </script>
 
 <style>
+    .page-top {
+        position:fixed;
+        z-index: 1;
+        width: 92%;
+    }
 
-
+    .tabs {
+        padding-top: 8%;
+    }
 </style>

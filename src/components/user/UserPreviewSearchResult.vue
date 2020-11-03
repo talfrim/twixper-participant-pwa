cb77cfc1e1f805f3d4551aee8e460ade1c86ed43
nirdz <nirdz@post.bgu.ac.il>
2020-11-04 00:11:35 +0200

    Improved user preview search

--- src/components/user/UserPreviewSearchResult.vue
+++ src/components/user/UserPreviewSearchResult.vue
@@ -1,33 +1,36 @@
 <template> 
-    <div class="post border"> 
-        <div class="user-avatar" v-lazyload>
-            <router-link :to="{ name: 'userPage', params: {userName: username} }"
-                tag="img" :data-url="profileImgUrl"> 
-            </router-link>
-        </div>
-       
-        <div class="user-content">
-            <div class="user-info">
-                <h4>
-                    {{name}} 
-                    <i v-if="verified" class="fas fa-check-circle"></i> 
-                    <div class="username">@{{username}}</div>
-                </h4>
+    <div class="wrapper"> 
+        <div class="user-avatar-container">
+            <div class="user-avatar" v-lazyload>
+                <router-link :to="{ name: 'userPage', params: {userName: username} }"
+                    tag="img" :data-url="profileImgUrl"> 
+                </router-link>
             </div>
-            <div class="follow-btn-container">
-                <!--<button style="height: 20px; margin-right: 10px">Follow / Unfollow</button>--> 
-                <FollowButton class="follow-btn"
-                    :usernameContext="username" 
-                    :following="following" 
-                />
+       </div>
+        <div class="user-content-wrapper">
+            <div class="top-segment">
+                <div class="names-container"> <!-- inline-block -->
+                    <div class="user-full-name-container">
+                        <span>{{name}}</span>
+                        <i v-if="isVerified" class="fas fa-check-circle"></i>
+                    </div>
+                    <div class="user-username-container">
+                        <span>@{{username}}</span>
+                    </div>
+                </div>
+                <div class="follow-btn-container"> <!-- inline-block -->
+                    <FollowButton class="follow-btn"
+                        :usernameContext="username" 
+                        :following="following" 
+                    />
+                </div>
             </div>
 			<div class="user-desc" >
-                    {{description}}
+                {{description}}
 		    </div>
         </div>
     </div>
 </template>
-FollowButton
 <script>
     import FollowButton from "../buttons/FollowButton.vue";
     export default {
@@ -41,7 +44,7 @@ FollowButton
                 username: "",
                 description: "",
                 profileImgUrl: "",
-                verified: false,
+                isVerified: false,
                 following:false,
             }
         },
@@ -59,7 +62,7 @@ FollowButton
             this.description = userPrev.description
             this.profileImgUrl = userPrev.profile_image_url_https
             this.isFollowed = userPrev.following  
-			this.verified = userPrev.verified
+			this.isVerified = userPrev.verified
         }
     }
 </script>
